4ef74e46b2666d6b64494d550f4827c0b3dcad83
Guilherme Padovani Silveira <64567292+GuilhermeSilveira93@users.noreply.github.com>
2023-05-22 17:15:40 -0300

    Update EditaMaquinas.jsx

--- PDS/pds-portocel/src/components/Telas/Cadastros/PDS/EditaMaquinas.jsx
+++ PDS/pds-portocel/src/components/Telas/Cadastros/PDS/EditaMaquinas.jsx
@@ -22,110 +22,125 @@ export default class EditaMaquinas extends Component {
     const { id } = this.props
     const { tipoIntervencao } = this.state
     let linhas = []
-    for (let index = 0; index < tipoIntervencao.length; index++) {
-      if (index < tipoIntervencao.length / 3) {
-        const codigo = tipoIntervencao[index]['Codigo'];
-        const tipo = tipoIntervencao[index]['Tipo Intervenção'];
-        const linha = <tr className="linhas"><td>{codigo}</td><td>{tipo}</td></tr>
-        linhas.push(linha)
-      } else {
-        const trlinhas = document.querySelector('.linhas')
-        console.log(trlinhas)
-        for (let index = 0; index < trlinhas.length; index++) {
-          const valor = trlinhas[index];
-          console.log(valor)
-          /*if (valor.children.length === 2) {
-            checkboxLabel.append(valores.itens)
-            checkbox.append(valores.itens)
-            tdCodigo.append(valores.codigo)
-            tdTipoIntervencao.append(checkbox)
-            tdTipoIntervencao.append(checkboxLabel)
-            valor.appendChild(tdCodigo)
-            valor.appendChild(tdTipoIntervencao)
-            divTipoIntervencao.append(valor)
-            break
-          } */
-        }
-
-
-      }
-      return (
-        <main>
-          <div id="header">
-            <h1>Pedido de Serviço - PDS</h1>
-            <fieldset>
-              <legend>Configurações da Manutenção</legend>
-              <form action="post">
-                <label htmlFor="controladorDeCargas" name="controladorDeCargas" id="controladorDeCargas">CONTROLADOR DE CARGAS
-                  :</label>
-                <input type="text" name="controladorDeCargas" id="controladorDeCargas" style={{ width: '72.2%' }} /><br />
-
-                <label htmlFor="operadorTPA" name="operadorTPA" id="operadorTPA">OPERADOR TPA
-                  :</label>
-                <input type="text" name="operadorTPA" id="operadorTPA"></input>
-
-                <label htmlFor="portoCel" name="portoCel" id="portoCel">PORTOCEL
-                  :</label>
-                <input type="text" name="portoCel" id="portoCel" />
-
-                <label htmlFor="jsl" name="jsl" id="jsl">JSL
-                  :</label>
-                <input type="text" name="jsl" id="jsl" /><br />
-
-                <label htmlFor="cracha" name="cracha" id="cracha"></label>
-
-                <label htmlFor="Frota" name="Frota" id="Frota"></label>
-
-                <label htmlFor="Horimetro" name="Horimetro" id="Horimetro">HORÍMETRO
-                  :</label>
-                <input type="number" name="Horimetro" id="Horimetro" />
-
-                <label htmlFor="Data" name="Data" id="Data">DATA: 10/05/2023 13:23</label>
-              </form>
-            </fieldset>
-            <div id="tipoIntervencao">
-              <table border="1">
-                <thead>
-                  <tr>
-                    <th>
-                      <h3>Codigo</h3>
-                    </th>
-                    <th>
-                      <h3>Tipo Intervenção</h3>
-                    </th>
-                    <th>
-                      <h3>Codigo</h3>
-                    </th>
-                    <th>
-                      <h3>Tipo Intervenção</h3>
-                    </th>
-                    <th>
-                      <h3>Codigo</h3>
-                    </th>
-                    <th>
-                      <h3>Tipo Intervenção</h3>
-                    </th>
-                  </tr>
-
-                </thead>
-                <tbody>
-                  {
-                    tipoIntervencao ?
-                      linhas
-                      : ''
-                  }
-
-                </tbody>
-              </table>
-            </div>
-            <div id="anotacoes">
-              <label htmlFor="anotacoes">Serviço a Executar</label><br />
-              <textarea name="anotacoes" id="anotacoes" cols="100" rows="10" style={{ resize: 'none' }}></textarea>
-            </div>
-          </div>
-        </main>
+    if (tipoIntervencao && tipoIntervencao.length > 0) {
+    let tamanho = Math.floor(tipoIntervencao.length / 3)
+    let resto = tipoIntervencao.length % 3
+    tamanho = resto > 0 ? tamanho + 1 : tamanho
+    for (let index = 0; index < tamanho; index++) {
+      console.log(index + " " + tamanho)
+      linhas.push(
+        <tr>
+          <td>{tipoIntervencao[index]['Codigo']}</td>
+          <td>{index + tamanho < tipoIntervencao.length ? tipoIntervencao[index + tamanho]['Codigo'] : ''}</td>
+          <td>{index + tamanho * 2 < tipoIntervencao.length ? tipoIntervencao[index + tamanho * 2]['Codigo'] : ''}</td>
+        </tr>
       )
     }
+    
+  } 
+    /*
+    if (index < tipoIntervencao.length / 3) {
+      const codigo = tipoIntervencao[index]['Codigo'];
+      const tipo = tipoIntervencao[index]['Tipo Intervenção'];
+      const linha = <tr className="linhas"><td>{codigo}</td><td>{tipo}</td></tr>
+      linhas.push(linha)
+    } else {
+      const trlinhas = document.querySelector('.linhas')
+      console.log(trlinhas)
+      for (let index = 0; index < trlinhas.length; index++) {
+        const valor = trlinhas[index];
+        console.log(valor)*/
+    /*if (valor.children.length === 2) {
+      checkboxLabel.append(valores.itens)
+      checkbox.append(valores.itens)
+      tdCodigo.append(valores.codigo)
+      tdTipoIntervencao.append(checkbox)
+      tdTipoIntervencao.append(checkboxLabel)
+      valor.appendChild(tdCodigo)
+      valor.appendChild(tdTipoIntervencao)
+      divTipoIntervencao.append(valor)
+      break
+    } */
 
+
+
+
+    return (
+      <main>
+        <div id="header">
+          <h1>Pedido de Serviço - PDS</h1>
+          <fieldset>
+            <legend>Configurações da Manutenção</legend>
+            <form action="post">
+              <label htmlFor="controladorDeCargas" name="controladorDeCargas" id="controladorDeCargas">CONTROLADOR DE CARGAS
+                :</label>
+              <input type="text" name="controladorDeCargas" id="controladorDeCargas" style={{ width: '72.2%' }} /><br />
+
+              <label htmlFor="operadorTPA" name="operadorTPA" id="operadorTPA">OPERADOR TPA
+                :</label>
+              <input type="text" name="operadorTPA" id="operadorTPA"></input>
+
+              <label htmlFor="portoCel" name="portoCel" id="portoCel">PORTOCEL
+                :</label>
+              <input type="text" name="portoCel" id="portoCel" />
+
+              <label htmlFor="jsl" name="jsl" id="jsl">JSL
+                :</label>
+              <input type="text" name="jsl" id="jsl" /><br />
+
+              <label htmlFor="cracha" name="cracha" id="cracha"></label>
+
+              <label htmlFor="Frota" name="Frota" id="Frota"></label>
+
+              <label htmlFor="Horimetro" name="Horimetro" id="Horimetro">HORÍMETRO
+                :</label>
+              <input type="number" name="Horimetro" id="Horimetro" />
+
+              <label htmlFor="Data" name="Data" id="Data">DATA: 10/05/2023 13:23</label>
+            </form>
+          </fieldset>
+          <div id="tipoIntervencao">
+            <table border="1">
+              <thead>
+                <tr>
+                  <th>
+                    <h3>Codigo</h3>
+                  </th>
+                  <th>
+                    <h3>Tipo Intervenção</h3>
+                  </th>
+                  <th>
+                    <h3>Codigo</h3>
+                  </th>
+                  <th>
+                    <h3>Tipo Intervenção</h3>
+                  </th>
+                  <th>
+                    <h3>Codigo</h3>
+                  </th>
+                  <th>
+                    <h3>Tipo Intervenção</h3>
+                  </th>
+                </tr>
+
+              </thead>
+              <tbody>
+                {
+                  tipoIntervencao ?
+                    linhas
+                    : ''
+                }
+
+              </tbody>
+            </table>
+          </div>
+          <div id="anotacoes">
+            <label htmlFor="anotacoes">Serviço a Executar</label><br />
+            <textarea name="anotacoes" id="anotacoes" cols="100" rows="10" style={{ resize: 'none' }}></textarea>
+          </div>
+        </div>
+      </main >
+    )
   }
+
 }
